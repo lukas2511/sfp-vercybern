291befbafad6e3bed3310ed50df73a40de94c2a5
Lukas Schauer <lukas@schauer.so>
2017-08-09 16:59:49 +0200

    also change part number and serial, fix checksum for extended id fields

--- fix-checksum.sh
+++ fix-checksum.sh
@@ -3,14 +3,20 @@
 eeprom -d /dev/i2c-9 -a 0x50 -p 1 -f /tmp/dump.bin >/dev/null 2>/dev/null
 
 sum=0
-
-for byte in $(dd if=/tmp/dump.bin bs=1 count=62 2>/dev/null | xxd -p -c 1); do
+for byte in $(dd if=/tmp/dump.bin bs=1 count=63 2>/dev/null | xxd -p -c 1); do
 	byte="$(printf "%d" "0x$byte")"
 	sum=$(((${sum} + ${byte}) % 256))
 done
+checksum1="$(printf "0x%02x" "${sum}")"
 
-checksum="$(printf "0x%02x" "${sum}")"
+sum=0
+for byte in $(dd if=/tmp/dump.bin bs=1 count=31 skip=64 2>/dev/null | xxd -p -c 1); do
+	byte="$(printf "%d" "0x$byte")"
+	sum=$(((${sum} + ${byte}) % 256))
+done
+checksum2="$(printf "0x%02x" "${sum}")"
 
 ./unlock.sh
 sleep 1
-i2cset -y 9 0x50 0x3f "${checksum}"
+i2cset -y 9 0x50 0x3f "${checksum1}"
+i2cset -y 9 0x50 0x5f "${checksum2}"
--- write.sh
+++ write.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
 
-#NEWSTR="CISCO-FINISAR"
-NEWSTR="CYBER-DISCO    "
+#NEWSTR="CISCO-FINISAR  "
+ VENDOR="CYBER CYBER CYB"
 
-if [[ ! "${#NEWSTR}" = "15" ]]; then
+#SERIAL="FNS17521U7B     "
+ SERIAL="CYBER CYBER CYBE"
+
+#PARTNO="FTLX8570D3BCL-C2A   "
+ PARTNO="CYBER CYBER CYBER CY"
+
+if [[ ! "${#VENDOR}" = "15" ]] || [[ ! "${#SERIAL}" = "16" ]] || [[ ! "${#PARTNO}" = "20" ]]; then
 	echo "NOOOPE!"
 	exit 1
 fi
 
+echo "Unlocking module"
 ./unlock.sh
 sleep 1
-writeaddr=20
 
-for chr in $(printf "${NEWSTR}" | xxd -p -c1); do
+# write vendor
+echo "Writing vendor"
+writeaddr=20
+for chr in $(printf "${VENDOR}" | xxd -p -c1); do
 	i2cset -y 9 0x50 0x$(printf "%02x" ${writeaddr}) 0x$chr
 	sleep 0.05
 	writeaddr=$((writeaddr+1))
 done
+
+# write part number
+echo "Writing part number"
+writeaddr=40
+for chr in $(printf "${PARTNO}" | xxd -p -c1); do
+	i2cset -y 9 0x50 0x$(printf "%02x" ${writeaddr}) 0x$chr
+	sleep 0.05
+	writeaddr=$((writeaddr+1))
+done
+
+# write serial
+echo "Writing serial"
+writeaddr=68
+for chr in $(printf "${SERIAL}" | xxd -p -c1); do
+	i2cset -y 9 0x50 0x$(printf "%02x" ${writeaddr}) 0x$chr
+	sleep 0.05
+	writeaddr=$((writeaddr+1))
+done
+
+echo "Fixing checksum"
+./fix-checksum.sh
+
+
 
